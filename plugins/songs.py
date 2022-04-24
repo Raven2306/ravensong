07f206dc4307b4b7fb594b51563dbaf7e27d54b4
Revan <101807432+Revan1704@users.noreply.github.com>
2022-04-24 18:49:30 +0400

    Update songs.py

--- plugins/songs.py
+++ plugins/songs.py
@@ -23,13 +23,13 @@ async def song_dl(_, msg: Message):
     if len(msg.command) == 1:
         return await msg.reply(text=text, parse_mode='md')
 
-    r_text = await msg.reply('Yerinə Yetirilir...')
+    r_text = await msg.reply('⚡️Axtarılır...')
     url = msg.text.split(None, 1)[1]
     url = extract_the_url(url=url)
     
     if url == 0:return await r_text.edit('I could not find that song. Try with another keywords...')
 
-    await r_text.edit('Yüklənir...')
+    await r_text.edit('⚡️Yüklənir...')
     ytinfo = descargar.get_song(url)
 
     if ytinfo == 0:
@@ -37,7 +37,7 @@ async def song_dl(_, msg: Message):
         return
 
     try:
-        await r_text.edit_text('Göndərilir...')
+        await r_text.edit_text('⚡️Göndərilir...')
     except MessageNotModified:
         pass
 
